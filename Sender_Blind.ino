672bd7e158429dfc1a7eb3b06423a6629abe1d32
1606micah <59399526+1606micah@users.noreply.github.com>
2025-01-04 15:25:41 -0800

    Update Sender_Blind.ino

--- Sender_Blind.ino
+++ Sender_Blind.ino
@@ -1,52 +1,34 @@
-/*
-  Rui Santos
-  Complete project details at https://RandomNerdTutorials.com/esp-now-esp32-arduino-ide/
-  
-  Permission is hereby granted, free of charge, to any person obtaining a copy
-  of this software and associated documentation files.
-  
-  The above copyright notice and this permission notice shall be included in all
-  copies or substantial portions of the Software.
-*/
 
 #include <esp_now.h>
 #include <WiFi.h>
 
-// REPLACE WITH YOUR RECEIVER MAC Address
 uint8_t broadcastAddress[] = {0x3c, 0x71, 0xbf, 0x62, 0xd4, 0xbc};
 
-// Structure example to send data
-// Must match the receiver structure
+
 typedef struct struct_message {
   int blind_status;
 } struct_message;
 
-// Create a struct_message called myData
 struct_message myData;
 
 esp_now_peer_info_t peerInfo;
 
-// callback when data is sent
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
   Serial.print("\r\nLast Packet Send Status:\t");
   Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
  
 void setup() {
-  // Init Serial Monitor
   Serial.begin(115200);
  
-  // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
-  // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-NOW");
     return;
   }
 
-  // Once ESPNow is successfully Init, we will register for Send CB to
-  // get the status of Transmitted packet
+
   esp_now_register_send_cb(OnDataSent);
   
   // Register peer
